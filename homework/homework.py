546d9af05a6c9938727d922a34de62589db99529
romz987pc <eesistance@gmail.com>
2024-10-26 18:13:22 +0300

    feat: задание 1

---
+++ homework/homework.py
@@ -0,0 +1,18 @@
+# Домашнее задание 
+
+def bill_hates():
+    """
+    Выводит на экран цитату Билла Гейтса
+
+    :return: None
+    """
+    author = "Bill Gates"
+
+    print(
+        '"Don\'t compare yourself with anyone in this world...\n'
+        ' if you do so, you are insulting yourself.\"\n'
+        f'{author.rjust(50)}'
+    )
+
+
+bill_hates()
