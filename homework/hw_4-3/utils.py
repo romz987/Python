fe47ddbf1bfc5ed5d64d63b15248508780beb65f
romz987pc <eesistance@gmail.com>
2024-11-05 21:58:55 +0300

    feat: get_reg_data without patterns

---
+++ homework/hw_4-3/utils.py
@@ -0,0 +1,17 @@
+def get_reg_data() -> list:
+    """
+    Возвращает список паттернов регулярных выражений
+    для проверки пользовательских данных
+
+    :return: список паттернов
+    """
+    patterns_list = [
+        'pattern-one',
+        'pattern-two',
+        'pattern-three'
+    ]
+
+    return patterns_list
+
+
+
