44189f7e13d96a892d82dac5edae834cc5c0ea23
romz987pc <eesistance@gmail.com>
2024-11-05 22:11:18 +0300

    feat: reg_check and check_unique_data mockups

--- homework/hw_4-3/utils.py
+++ homework/hw_4-3/utils.py
@@ -14,4 +14,26 @@ def get_reg_data() -> list:
     return patterns_list
 
 
+def reg_check(user_data: str, reg_pattern: str, users_list: list, data_to_check=None) -> str:
+    """ 
+    Возвращает аргумент user_data, если он прошел все 
+    проверки
 
+    :param user_data: пользовательские данные для проверки
+    :param reg_pattern: паттерн регулярки для проверки данных
+    :param users_list: список уже созданных пользователей
+    :data_to_check:
+    :return: user_data
+    """
+    pass 
+
+
+def check_unique_data(user_data: str, ):
+    """ 
+    Проверяет уникальность введенного номера телефона 
+    и адреса email
+
+    :param user_data: пользовательские данные для проверки
+    :param 
+    """
+    pass
