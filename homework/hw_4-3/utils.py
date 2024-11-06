b1aea5e7195ad938e9e3f7239e7dccf0a5154736
romz987pc <eesistance@gmail.com>
2024-11-06 21:02:35 +0300

    feat: работает, но проверяет все данные на уникальность, вместо номера и email

--- homework/hw_4-3/utils.py
+++ homework/hw_4-3/utils.py
@@ -1,20 +1,23 @@
-def get_reg_data() -> list[dict]:
+import re
+
+
+def get_reg_data() -> dict:
     """
     Возвращает список паттернов регулярных выражений
     для проверки пользовательских данных
 
     :return: список паттернов
     """
-    patterns_list = [
-        {'name_pattern': r'^[a-zA-Zа-яА-ЯёЁ]+$'},
-        {'phone_pattern': r'^\+\d{1,3}\(\d{2}\)\d{7}$'},
-        {'email_pattern': r'^[a-zA-Z0-9]+@yandex\.ru$'}
-    ]
+    patterns_list = {
+        'name_pattern': r'^[a-zA-Zа-яА-ЯёЁ]+$',
+        'phone_pattern': r'^\+\d{1,3}\(\d{2}\)\d{7}$',
+        'email_pattern': r'^[a-zA-Z0-9]+@yandex\.ru$'
+    }
 
     return patterns_list
 
 
-def reg_check(user_data: str, reg_pattern: str, users_list: list, data_to_check=None) -> str:
+def reg_check(user_data: str, reg_pattern: str, users_list: list) -> str:
     """ 
     Возвращает аргумент user_data, если он прошел все 
     проверки
@@ -25,10 +28,16 @@ def reg_check(user_data: str, reg_pattern: str, users_list: list, data_to_check=
     :data_to_check:
     :return: user_data
     """
-    pass 
+    check_reg = bool(re.match(reg_pattern, user_data))
+    check_unique = check_unique_data(user_data, users_list)
+
+    if check_reg and check_unique:
+        return user_data
+
+    
 
 
-def check_unique_data(user_data: str, ):
+def check_unique_data(user_data: str, users_list: list):
     """ 
     Проверяет уникальность введенного номера телефона 
     и адреса email
@@ -36,4 +45,5 @@ def check_unique_data(user_data: str, ):
     :param user_data: пользовательские данные для проверки
     :param 
     """
-    pass
+    if user_data not in users_list:
+        return True
